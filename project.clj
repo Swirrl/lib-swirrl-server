90c2c13787d218c0119ea69d616c52843d1d54d2
Rick Moynihan <rick.m@swirrl.com>
2015-05-11 16:56:15 +0100

    Add dependency for compojure

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.3.2"]]
+                 [ring "1.3.2"]
+                 [compojure "1.3.4"]]
 
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
                                   [ring/ring-devel "1.3.2"]]}})
