47935752b5cfd0c689beec66170432984f5d50f0
Rick Moynihan <rick.m@swirrl.com>
2015-05-11 17:02:59 +0100

    add description & URL to project.clj to suppress warnings

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject swirrl/lib-swirrl-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Common library for code used in Swirrl servers"
+  :url "https://github.com/Swirrl/lib-swirrl-server/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
