7c7ace9a3b816403a5f7ac20adc85e5cf7168111
Rick Moynihan <rick.m@swirrl.com>
2016-01-28 15:28:45 +0000

    Breaking changes: Change response code and function signature for error-response.
    Also introduce use of prismatic schema for defining expected error
    objects etc.

--- src/swirrl_server/responses.clj
+++ src/swirrl_server/responses.clj
@@ -1,9 +1,20 @@
 (ns swirrl-server.responses
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [schema.core :as s]))
+
+(def SwirrlResponse {:type (s/enum :ok :error)})
+
+(def SwirrlError (merge SwirrlResponse {:type (s/eq :error)
+                                        :error s/Keyword
+                                        :message s/Str}))
+
+(def OkResponse {:type (s/eq :ok)})
 
 (def default-response-map {:type :ok})
 
-(def default-error-map {:type :error :message "An unknown error occured"})
+(def default-error-map {:type :error
+                        :error :unknown-error
+                        :message "An unknown error occured"})
 
 (defn api-response
   "Return a HTTP response of type code, with a JSON content type, and the given
@@ -17,16 +28,33 @@
 {:type :ok}"} ok-response (api-response 200 {:type :ok}))
 
 (defn not-found-response [message]
-  (api-response 404 {:type :not-found :message message}))
+  (api-response 404 {:type :error
+                     :error :not-found
+                     :message message}))
 
-(defn error-response
-  [code map]
-  (api-response code (merge default-error-map map)))
+(s/defn error-response :- SwirrlError
+  "Build a ring response containing a JSON error object."
+  ([]
+   (error-response 500))
 
-(defn bad-request-response
+  ([code]
+   (error-response code :unknown-error))
+
+  ([code error-type]
+   (error-response code error-type (:message default-error-map)))
+
+  ([code error-type msg]
+   (error-response code error-type msg {}))
+
+  ([code error-type msg map]
+   (api-response code (assoc (merge default-error-map map)
+                             :error error-type
+                             :message msg))))
+
+(s/defn bad-request-response :- SwirrlError
   "Returns a 'bad request' response from the given error message."
   [s]
-  (error-response 400 {:type :error :message s}))
+  (error-response 422 :invalid-parameters s))
 
 (defmacro when-params
   "Simple macro that takes a set of paramaters and tests that they're
