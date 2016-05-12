f6bdc2ed602adcfbfd86d873cb50a2b84857b6b9
lkitching <lee@swirrl.com>
2016-05-12 17:41:55 +0100

    Add functions for completing jobs successfully or by error.
    Add schemas for failed and completed job results and add functions for
    completing jobs successfully or by error. Make the existing
    complete-job! private to require all jobs are completed through one of
    these functions.

--- src/swirrl_server/async/jobs.clj
+++ src/swirrl_server/async/jobs.clj
@@ -22,12 +22,17 @@
           f
           (promise))))
 
+(defn job-completed?
+  "Whether the given job has been completed"
+  [job]
+  (realized? (:value-p job)))
+
 (defn create-child-job
   "Creates a continuation job from the given parent."
   [job child-fn]
   (assoc job :function child-fn :time (System/currentTimeMillis)))
 
-(defn complete-job!
+(defn- complete-job!
   "Adds the job to the state map of finished-jobs and delivers the
   supplied result to the jobs promise, which will cause blocking jobs
   to unblock, and give job consumers the ability to receive the
@@ -38,6 +43,45 @@
     (swap! finished-jobs assoc job-id promis)
     result))
 
+(def FailedJobResult
+  {:type (s/eq "error")
+   :message s/Str
+   :error-class s/Str
+   (s/optional-key :details) {s/Any s/Any}})
+
+(def SuccessfulJobResult
+  {:type (s/eq "ok")
+   (s/optional-key :details) {s/Any s/Any}})
+
+(def JobResult
+  (s/either FailedJobResult SuccessfulJobResult))
+
+(defn- failed-job-result [ex]
+  (let [result {:type "error"
+                :message (.getMessage ex)
+                :error-class (.getName (class ex))}]
+    (if (instance? clojure.lang.ExceptionInfo ex)
+      (assoc result :details (ex-data ex))
+      result)))
+
+(defn job-failed!
+  "Mark the given job as failed"
+  [job ex]
+  {:pre [(not (job-completed? job))]
+   :post [(job-completed? job)]}
+  (complete-job! job (failed-job-result ex)))
+
+(defn job-succeeded!
+  "Marks the given job as succeeded. If a details value is provided it
+  will be added to the job result map under the :details key."
+  ([job]
+   {:pre [(not (job-completed? job))]
+    :post [(job-completed? job)]}
+   (complete-job! job {:type "ok"}))
+  ([job details]
+   {:pre [(not (job-completed? job))]
+    :post [(job-completed? job)]}
+   (complete-job! job {:type "ok" :details details})))
 
 (def JobSubmittedResponse (merge OkObject
                                  {:finished-job s/Str
--- test/swirrl_server/async/jobs_test.clj
+++ test/swirrl_server/async/jobs_test.clj
@@ -1,6 +1,7 @@
 (ns swirrl-server.async.jobs-test
   (:require [swirrl-server.async.jobs :refer :all]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [schema.core :as s]))
 
 (deftest create-child-job-test
   (let [parent-fn #(println "parent")
@@ -11,3 +12,54 @@
       (is (= child-fn (:function child-job)) "Failed to update job function")
       (is (> (:time child-job) (:time parent-job)) "Failed to update job time")
       (is (= (:id parent-job) (:id child-job)) "Job id should not change from parent's"))))
+
+(defn- get-job-result [job]
+  @(:value-p job))
+
+(defn- assert-failure-result [job expected-message expected-class expected-details]
+  (let [{:keys [message error-class details] :as result} (get-job-result job)]
+    (s/validate FailedJobResult result)
+    (is (= expected-message message))
+    (is (= (.getName expected-class) error-class))
+    (is (= expected-details details))))
+
+(deftest job-completed?-test
+  (testing "Completed"
+    (let [{:keys [value-p] :as job} (create-job (fn []))]
+      (deliver value-p "completed")
+      (is (= true (job-completed? job)))))
+  (testing "Not completed"
+    (let [job (create-job (fn []))]
+      (is (= false (job-completed? job))))))
+
+(deftest job-failed-test
+  (testing "Java exception"
+    (let [job (create-job (fn []))
+          msg "Failed :("
+          ex (IllegalArgumentException. msg)]
+      (job-failed! job ex)
+      (assert-failure-result job msg IllegalArgumentException nil)))
+
+  (testing "ExceptionInfo"
+    (let [job (create-job (fn []))
+          msg "Job failed"
+          details {}
+          ex (ex-info msg details)]
+      (job-failed! job ex)
+      (assert-failure-result job msg clojure.lang.ExceptionInfo details))))
+
+(deftest job-succeeded-test
+  (testing "With details"
+    (let [job (create-job (fn []))
+          details {:foo :bar}]
+      (job-succeeded! job details)
+      (let [result (get-job-result job)]
+        (s/validate SuccessfulJobResult result)
+        (is (= details (:details result))))))
+
+  (testing "Without details"
+    (let [job (create-job (fn []))]
+      (job-succeeded! job)
+      (let [result (get-job-result job)]
+        (s/validate SuccessfulJobResult result)
+        (is (= false (contains? result :details)))))))
