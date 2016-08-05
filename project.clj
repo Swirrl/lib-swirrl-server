dccba6193bc453a7ba5be3d86a7a3ebad0032130
Rick Moynihan <rick.m@swirrl.com>
2016-08-05 17:50:18 +0100

    Version 0.6.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swirrl/lib-swirrl-server "0.6.3"
+(defproject swirrl/lib-swirrl-server "0.6.4-SNAPSHOT"
   :description "Common library for code used in Swirrl servers"
   :url "https://github.com/Swirrl/lib-swirrl-server/"
   :license {:name "Eclipse Public License"
